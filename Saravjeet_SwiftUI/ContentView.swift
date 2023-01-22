d81e79f330c63b61861a083042f28a8511598a62
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2023-01-22 01:30:28 -0500

    Initial Commit

---
+++ Saravjeet_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Saravjeet_SwiftUI
+//
+//  Created by Saravjeet Singh on 2023-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Saravjeet_SwiftUI/Saravjeet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saravjeet_SwiftUIApp.swift
+//  Saravjeet_SwiftUI
+//
+//  Created by Saravjeet Singh on 2023-01-22.
+//
+
+import SwiftUI
+
+@main
+struct Saravjeet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
